23ef13886c2b3a5e749924977008ef77c4ddaf25
Dhin069 <dhinosh69@gmail.com>
2025-02-04 20:59:39 +0530

    whatsapp

---
+++ index.js
@@ -0,0 +1,22 @@
+const arr = [
+    {
+        "name":"Bruno",
+        "profile":"./assets/images/bruno.jpg",
+        "text":"Hi",
+        "time":"02:01"
+    },
+    {
+        "name":"Bruno",
+        "profile":"./assets/images/bruno.jpg",
+        "text":"Hi",
+        "time":"02:01"
+    },
+    {
+        "name":"Bruno",
+        "profile":"./assets/images/bruno.jpg",
+        "text":"Hi",
+        "time":"02:01"
+    }
+]
+
+document.getElementById("chat-list").innerHTML="<h1>hi</h1>"
